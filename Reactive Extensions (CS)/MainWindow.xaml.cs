056c88cc68d38381914f6ea4f224d97dc7b4f84c
benoit.goethals <benoit.goethals@gmail.com>
2017-12-01 09:44:08 +0100

    init

---
+++ Reactive Extensions (CS)/MainWindow.xaml.cs
@@ -0,0 +1,106 @@
+﻿using Reactive_Extensions__CS_.DictionaryService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Reactive_Extensions__CS_
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            SubscribeToTextChanged();
+            SubscribeToSelectionChanged();
+        }
+
+        private DictServiceSoapClient service = new DictServiceSoapClient("DictServiceSoap");
+        private DictServiceSoapClient Service
+        {
+            get
+            {
+                return service;
+            }
+        }
+
+        private IObservable<WordDefinition> DefinitionCall(string word)
+        {
+            return Observable.FromAsync(() => Service.DefineAsync(word));
+        }
+
+
+        private IObservable<DictionaryWord[]> ServiceCall(string term)
+        {
+            return Observable.FromAsync(
+                                  () => Service.MatchInDictAsync("wn", term, "prefix"));
+        }
+
+        private void SubscribeToTextChanged()
+        {
+            var input = (from e in Observable.FromEventPattern(inputBox, "TextChanged")
+                         let s = ((TextBox)e.Sender).Text
+                         where s.Length > 1
+                         select s)
+                         .Throttle(TimeSpan.FromSeconds(0.5)).DistinctUntilChanged();
+
+
+            var result = from term in input
+                         from w in ServiceCall(term).TakeUntil(input)
+                         select w;
+
+            result.ObserveOn(wordsBox).Subscribe(
+              (words) => {
+                  wordsBox.ItemsSource = words;
+                  wordsBox.DisplayMemberPath = "Word";
+              },
+              (ex) => MessageBox.Show(ex.Message)
+            );
+
+        }
+
+
+        private void SubscribeToSelectionChanged()
+        {
+            var input = (from e in Observable.FromEventPattern(wordsBox, "SelectionChanged")
+                         let w = ((DictionaryWord)((ListBox)e.Sender).SelectedValue)
+                         select w).
+                         Throttle(TimeSpan.FromSeconds(0.5)).DistinctUntilChanged();
+
+            var res = from term in input
+                      where term != null
+                      from definition in DefinitionCall(term.Word).TakeUntil(input)
+                      select definition;
+
+            res.ObserveOn(resultBox).Subscribe(
+              (term) => {
+                  resultBox.Text = "";
+                  foreach (var definition in term.Definitions)
+                  {
+                      resultBox.Text += definition.WordDefinition + Environment.NewLine + Environment.NewLine;
+                  }
+              }
+            );
+        }
+
+        
+    }
+}
---
+++ Reactive Extensions (VB)/MainWindow.xaml.vb
@@ -0,0 +1,75 @@
+﻿Imports System.Reactive.Linq
+Imports System.Data
+Imports Reactive_Extensions__VB_.DictionaryService
+
+
+'  /// <summary>
+'  /// Interaction logic for MainWindow.xaml
+'  /// </summary>
+Class MainWindow
+    Private Sub Window_Loaded(sender As Object, e As RoutedEventArgs)
+        SubscribeToTextChanged()
+        SubscribeToSelectionChanged()
+    End Sub
+
+
+
+    Private _service As New DictServiceSoapClient("DictServiceSoap")
+    Private ReadOnly Property Service As DictServiceSoapClient
+        Get
+            Return _service
+        End Get
+
+    End Property
+
+
+
+    Private Function ServiceCall(term As String) As IObservable(Of DictionaryWord())
+        Return Observable.FromAsync(Function() Service.MatchInDictAsync("wn", term, "prefix"))
+    End Function
+
+    Private Sub SubscribeToTextChanged()
+        Dim input = (From e In Observable.FromEventPattern(inputBox, "TextChanged")
+                     Let s = DirectCast(e.Sender, TextBox).Text
+                     Where s.Length > 1
+                     Select s).Throttle(TimeSpan.FromSeconds(0.5)).DistinctUntilChanged()
+
+        Dim result = From term In input
+                     From w In ServiceCall(term).TakeUntil(input)
+                     Select w
+
+        result.ObserveOn(wordsBox).Subscribe(
+                Sub(words)
+                    wordsBox.ItemsSource = words
+                    wordsBox.DisplayMemberPath = "Word"
+                End Sub,
+                Sub(ex) MessageBox.Show(ex.Message)
+        )
+    End Sub
+
+    Private Function DefinitionCall(word As String) As IObservable(Of WordDefinition)
+        Return Observable.FromAsync(Function() Service.DefineAsync(word))
+    End Function
+
+    Private Sub SubscribeToSelectionChanged()
+        Dim input = (From e In Observable.FromEventPattern(wordsBox, "SelectionChanged")
+                     Let w = DirectCast(DirectCast(e.Sender, ListBox).SelectedValue, DictionaryWord)
+                     Select w).Throttle(TimeSpan.FromSeconds(0.5)).DistinctUntilChanged()
+
+
+        Dim res = From term In input
+                  Where term IsNot Nothing
+                  From definition In DefinitionCall(term.Word).TakeUntil(input)
+                  Select definition
+
+        res.ObserveOn(resultBox).Subscribe(
+        Sub(term)
+            resultBox.Text = ""
+            For Each definition In term.Definitions
+                resultBox.Text += definition.WordDefinition + Environment.NewLine + Environment.NewLine
+            Next
+        End Sub
+        )
+
+    End Sub
+End Class
